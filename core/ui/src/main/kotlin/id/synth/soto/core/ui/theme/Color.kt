9a2d2e48f449ac579c37f95cd7e2166916c1fd0e
Yuuki Rika <yvvki@duck.com>
2024-02-13 14:27:07 +0700

    Update theme

---
+++ core/ui/src/main/kotlin/id/synth/soto/core/ui/theme/Color.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 Soto contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless requiTertiary by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package id.synth.soto.core.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF585123)
+val Primary10 = Color(0XFF201C00)
+val Primary20 = Color(0XFF373100)
+val Primary30 = Color(0XFF504700)
+val Primary40 = Color(0XFF6A5F00)
+val Primary80 = Color(0XFFDAC84E)
+val Primary90 = Color(0XFFF7E467)
+
+val Secondary = Color(0xFFEEC170)
+val Secondary10 = Color(0XFF271900)
+val Secondary20 = Color(0XFF412D00)
+val Secondary30 = Color(0XFF5E4200)
+val Secondary40 = Color(0XFF7C5800)
+val Secondary80 = Color(0XFFF8BD48)
+val Secondary90 = Color(0XFFFFDEA8)
+
+val Tertiary = Color(0xFFDB5461)
+val Tertiary10 = Color(0XFF40000C)
+val Tertiary20 = Color(0XFF680018)
+val Tertiary30 = Color(0XFF8B172B)
+val Tertiary40 = Color(0XFFAB3140)
+val Tertiary80 = Color(0XFFFFB3B5)
+val Tertiary90 = Color(0XFFFFDADA)
+
+val Error10 = Color(0xFF410002)
+val Error20 = Color(0xFF93000A)
+val Error30 = Color(0xFF690005)
+val Error40 = Color(0xFFBA1A1A)
+val Error80 = Color(0xFFFFB4AB)
+val Error90 = Color(0xFFFFDAD6)
+
+val Neutral = Color(0xFFE3F2FD)
+val Neutral10 = Color(0XFF001F2A)
+val Neutral20 = Color(0xFF003547)
+val Neutral90 = Color(0xFFBFE9FF)
+val Neutral95 = Color(0xFFBFE9FF)
+val Neutral99 = Color(0XFFFAFCFF)
+
+val NeutralVariant30 = Color(0xFF4A4739)
+val NeutralVariant50 = Color(0xFF7B7768)
+val NeutralVariant60 = Color(0xFF959181)
+val NeutralVariant80 = Color(0xFFCCC6B5)
+val NeutralVariant90 = Color(0xFFE8E2D0)
--- core/ui/src/main/kotlin/id/synth/soto/core/ui/theme/Theme.kt
+++ core/ui/src/main/kotlin/id/synth/soto/core/ui/theme/Theme.kt
@@ -27,10 +27,77 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
+val LightColorScheme = lightColorScheme(
+    primary = Primary40,
+    onPrimary = Color.White,
+    primaryContainer = Primary90,
+    onPrimaryContainer = Primary10,
+    inversePrimary = Primary80,
+    secondary = Secondary40,
+    onSecondary = Color.White,
+    secondaryContainer = Secondary90,
+    onSecondaryContainer = Secondary10,
+    tertiary = Tertiary40,
+    onTertiary = Color.White,
+    tertiaryContainer = Tertiary90,
+    onTertiaryContainer = Tertiary10,
+    background = Neutral99,
+    onBackground = Neutral10,
+    surface = Neutral99,
+    onSurface = Neutral10,
+    surfaceVariant = NeutralVariant90,
+    onSurfaceVariant = NeutralVariant30,
+    surfaceTint = Primary40,
+    inverseSurface = Neutral20,
+    inverseOnSurface = Neutral95,
+    error = Error40,
+    onError = Color.White,
+    errorContainer = Error90,
+    onErrorContainer = Error10,
+    outline = NeutralVariant50,
+    outlineVariant = NeutralVariant80,
+    scrim = Color.Black,
+)
+
+
+val DarkColorScheme = darkColorScheme(
+    primary = Primary80,
+    onPrimary = Primary20,
+    primaryContainer = Primary30,
+    onPrimaryContainer = Primary90,
+    inversePrimary = Primary40,
+    secondary = Secondary80,
+    onSecondary = Secondary20,
+    secondaryContainer = Secondary30,
+    onSecondaryContainer = Secondary90,
+    tertiary = Tertiary80,
+    onTertiary = Tertiary20,
+    tertiaryContainer = Tertiary30,
+    onTertiaryContainer = Tertiary90,
+    background = Neutral10,
+    onBackground = Neutral90,
+    surface = Neutral10,
+    onSurface = Neutral90,
+    surfaceVariant = NeutralVariant30,
+    onSurfaceVariant = NeutralVariant80,
+    surfaceTint = Primary80,
+    inverseSurface = Neutral90,
+    inverseOnSurface = Neutral10,
+    error = Error80,
+    onError = Error30,
+    errorContainer = Error20,
+    onErrorContainer = Error90,
+    outline = NeutralVariant60,
+    outlineVariant = NeutralVariant30,
+    scrim = Color.Black,
+)
+
 
 @Composable
 fun SotoTheme(
@@ -45,8 +112,8 @@ fun SotoTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> darkColorScheme()
-        else -> lightColorScheme()
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
