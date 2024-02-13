86ddb713dc22e3dd945d7dc78bcf66748706c7ff
Yuuki Rika <yvvki@duck.com>
2024-02-13 16:36:16 +0700

    Implement navigation in main

--- app/src/main/kotlin/id/synth/soto/MainActivity.kt
+++ app/src/main/kotlin/id/synth/soto/MainActivity.kt
@@ -20,15 +20,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
-import id.synth.soto.core.ui.theme.SotoTheme
+import id.synth.soto.ui.SotoApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -36,30 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SotoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            SotoApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SotoTheme {
-        Greeting("Android")
     }
 }
