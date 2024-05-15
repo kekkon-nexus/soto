fa6946308910b4acc6962dbcb8822fb7e81b62f9
Yuuki Rika <yvvki@duck.com>
2024-05-15 09:31:57 +0700

    Update Kotlin to RC-3 and migrate compose compiler

--- buildSrc/src/main/kotlin/id/synth/soto/convention/build/kotlin.plugin.compose.gradle.kts
+++ buildSrc/src/main/kotlin/id/synth/soto/convention/build/kotlin.plugin.compose.gradle.kts
@@ -14,25 +14,8 @@
  * limitations under the License.
  */
 
-package id.synth.soto.convention
-
-import id.synth.soto.convention.build.dsl.android
-import id.synth.soto.convention.build.dsl.libs
+package id.synth.soto.convention.build
 
 plugins {
-    id("id.synth.soto.convention.build.kotlin.plugin.compose")
-}
-
-android {
-    buildFeatures {
-        compose = true
-    }
-}
-
-dependencies {
-    val composeBom = platform(libs.androidx.compose.bom)
-    add("implementation", composeBom)
-    add("androidTestImplementation", composeBom)
-
-    add("implementation", libs.androidx.compose.material3)
+    org.jetbrains.kotlin.plugin.compose
 }
